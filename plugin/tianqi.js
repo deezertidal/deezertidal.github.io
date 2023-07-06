63c8eeed30494dbb3970b5231031594096b8ca07
deezertidal <jrull5400@gmail.com>
2023-07-06 18:26:35 +0800

    .

--- plugin/tianqi.js
+++ plugin/tianqi.js
@@ -19,7 +19,8 @@ $httpClient.get(apiUrl, (error, response, data) => {
 
   const cityInfo = weatherData.cityInfo;
   const currentWeather = weatherData.data.forecast[0];
-  const message = `城市：${cityInfo.city}\n更新时间：${cityInfo.updateTime} \n天气：${currentWeather.type}\n温度：${currentWeather.low}  ${currentWeather.high}\n湿度：${weatherData.data.shidu}\n空气质量：${weatherData.data.quality}\nPM2.5：${weatherData.data.pm25}\nPM10：${weatherData.data.pm10}\n风向：${currentWeather.fx}\n风力：${currentWeather.fl}\n日出时间：${currentWeather.sunrise}\n日落时间：${currentWeather.sunset}\nTips：${currentWeather.notice}`;
+    const message = `📍城市：${cityInfo.city}\n🕰︎更新时间：${cityInfo.updateTime} \n🌤︎天气：${currentWeather.type}\n🌡︎温度：${currentWeather.low}  ${currentWeather.high}\n💧湿度：${weatherData.data.shidu}\n💨空气质量：${weatherData.data.quality}\n☁️PM2.5：${weatherData.data.pm25}\n☁️PM10：${weatherData.data.pm10}\n🪁风向：${currentWeather.fx}\n🌪️风力：${currentWeather.fl}\n🌅日出时间：${currentWeather.sunrise}\n🌇日落时间：${currentWeather.sunset}\n🏷︎Tips：${currentWeather.notice}`;
+
 
   $notification.post('天气通知', '', message);
   $done();
